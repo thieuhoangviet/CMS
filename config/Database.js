0f61c5eedfed37d7ccd5aa285c425ac3318e1172
thlam90 <hoanglamcnttkimchi@gmail.com>
2024-05-20 11:06:21 +0700

    Update project

---
+++ config/Database.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const mongoURI = 'mongodb+srv://hoangviet:wqnwXzNFNCcqH21g@cluster0.a7ff2lm.mongodb.net/CMS?retryWrites=true&w=majority&appName=Cluster0';
+  
+  const connectDB = async () => {
+    try {
+      await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+      console.log('Kết nối thành công');
+    } catch (err) {
+      console.log('Database connection error:', err);
+      process.exit(1);
+    }
+  };
+  
+  module.exports = connectDB;
+  
---
+++ models/User.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    default: 'user'
+  },
+  verificationCode: {
+    type: String,
+    required: false
+  },
+  emailVerified: {
+    type: Boolean,
+    default: false
+  }
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
