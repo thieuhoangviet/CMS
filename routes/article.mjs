a987b1dfeaef2c9601dd05cd42c6c71031495944
luung <luunguyentrungtri@gmail.com>
2024-05-24 11:21:41 +0700

    Fix issue article

--- routes/article.mjs
+++ routes/article.mjs
@@ -1,9 +1,33 @@
 import express from 'express';
 import slugify from 'slugify';
-import { Articale } from '../models/article.js';
-import upload from '../utils/imageUpload.js';
+import  Articale  from '../models/article.mjs';
+import upload from '../utils/imageUpload.mjs';
 const app = express.Router();
 
+//get
+app.get('/', (req,res) => {
+    return res.status(200).send({message: 'hello'})
+})
+//create article
+app.post('/',upload.single('image'),async (req,res) => {
+    try {
+        const article = new Articale({
+            title: req.body.title,
+            content: req.body.content,
+            article_slug: slugify(req.body.title,{ lower: true }),
+            draft: req.body.draft,
+            excerpt: req.body.excerpt,
+            published: req.body.published,
+            image: req.file.filename
+        });
+        
+        await article.save();
+        return res.status(200).json(article);
+    } catch (error) {
+        return res.status(500).json({ error: error });
+    }
+  
+})
 //update article
 app.put('/:id',upload.single('image') , async (req, res) => {
     try {
