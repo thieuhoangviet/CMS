5e1494945075e3c2f619cd86a7fc3baad4c89039
duyvo <duyvo3001@gmail.com>
2024-06-01 15:12:17 +0700

    update cors

--- app.mjs
+++ app.mjs
@@ -19,6 +19,24 @@ connectDB();
 // Initialize Express app
 const app = express();
 
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to w
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 // Define __filename and __dirname
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
